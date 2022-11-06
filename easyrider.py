e7da59b801817d65fb3e3f4a4aa9152d2086aa14
Rodriguez <sergerodriguez.education@gmail.com>
2022-11-06 15:51:29 +0500

    feat: step 1 compiled
    Description
    You just started sorting out the existing database of the "Easy Rider" bus company. As you take the first look at the data, you realize that it's not going to be easy.
    
    Sometimes numbers are missing from where they should definitely be. You also noticed that sometimes there are too many or too few characters. Fortunately, there is documentation to help you sort out this mess. However, this documentation is not a hundred percent complete: part of it was torn away when your colleague spilled coffee on it. Let's see what we can make out.
    
    Here are the documents that you have: documentation and diagram of the bus lines.
    
    Objectives
    The string containing the data in JSON format is passed to standard input.
    Check that the data types match.
    Check that the required fields are filled in.
    Display the information about the number of found errors in total and in each field. Keep in mind that there might be no errors at all.
    The output should have the same formatting as shown in the example.
    No need to worry about the format (name validation) now. Let's at first just make sure that the fields have the right type and all required ones are filled.
    If you can't find the necessary information in the stage description, it can probably be found in the attached documentation.
    
    Note that the type Char is present among the data types.

--- easyrider.py
+++ easyrider.py
@@ -1,1 +1,56 @@
-# Write your code here
+import json
+
+rules = {
+    "bus_id": {"type": "int", "required": True, "value": b''},
+    "stop_id": {"type": "int", "required": True, "value": b''},
+    "stop_name": {"type": "str", "required": True, "value": b''},
+    "next_stop": {"type": "int", "required": True, "value": b''},
+    "stop_type": {"type": "char", "required": False, "value": [b'S', b'O', b'F', b'']},
+    "a_time": {"type": "str", "required": True, "value": b''}
+}
+
+output = {
+    "bus_id": 0,
+    "stop_id": 0,
+    "stop_name": 0,
+    "next_stop": 0,
+    "stop_type": 0,
+    "a_time": 0
+}
+
+
+def check_type(el):
+    if isinstance(el, int):
+        return "int"
+    elif isinstance(el, str):
+        return "str"
+
+
+def check_rules(block_, rules_):
+    for key, value in block_.items():
+        if rules_[key]["required"]:
+            this_type = rules_[key]["type"]
+            this_value = rules_[key]["value"]
+            if any([check_type(value) != this_type, (str(value)).encode() == this_value]):
+                output[key] += 1
+        else:
+            if str(value).encode() not in rules_[key]["value"]:
+                output[key] += 1
+
+
+def count_errors(out_):
+    return sum(out_.values())
+
+
+def output_print():
+    print(f"Type and required field validation: {count_errors(output)} errors")
+    for key, val in output.items():
+        print(f"{key}: {val}")
+
+
+data = json.loads(input())
+
+for block in data:
+    check_rules(block, rules)
+
+output_print()
